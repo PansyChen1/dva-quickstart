c2de935df2c0ff397118eb0d785c5de62646773f
chenping <chenping@wecash.net>
2019-11-18 17:02:39 +0800

    add ProductList component

--- src/router.js
+++ src/router.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import IndexPage from './routes/IndexPage';
+import Products from './routes/Products';
+import ProductList from './components/ProductList';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
         <Route path="/" exact component={IndexPage} />
+        <Route path="/products" exact component={Products} />
+        <Route path="/productList" exact component={ProductList} />
       </Switch>
     </Router>
   );
