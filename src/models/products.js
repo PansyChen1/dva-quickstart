8162fe61986d8cbf2461a7f4c8991d233ecea75c
chenping <chenping@wecash.net>
2019-11-19 14:56:18 +0800

    add modal for adding products

--- src/models/products.js
+++ src/models/products.js
@@ -1,9 +1,16 @@
+import {Modal} from 'antd';
+import {useState} from 'react';
+
 export default {
   namespace: 'products',
   state: [],
   reducers: {
-    'delete'(state, { payload: id }) {
+    'delete'(state, {payload: id}) {
+      console.log("state:", state);
       return state.filter( item => item.id !== id);
     },
+    'add'(state, {payload: id}) {
+      return state.push(item => item.id);
+    }
   },
 };
--- src/routes/Products.js
+++ src/routes/Products.js
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "dva";
 import ProductList from '../components/ProductList';
+import {Button, Icon, Modal, Input} from "antd";
 
 const Products = ({ dispatch, products }) => {
+
+  const [visible, setVisible] = useState(false);
+
+  function handleCancel() {
+    setVisible(!visible);
+  }
+
+  function handleOk(id) {
+    dispatch({
+      type: 'products/add',
+      payload: id,
+    });
+  }
+
+  const AddProducts = ({visible}) => {
+    return (
+      <Modal
+        title={'Add a new products'}
+        visible={visible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <span>Product Name : </span><Input style={{width: 350, marginLeft: 10}} placeholder={"Please input the Product Name"}/>
+      </Modal>
+    )
+  };
+
   function handleDelete(id) {
     dispatch({
       type: 'products/delete',
       payload: id,
     });
   }
+
   return (
     <div>
       <h2>List of Products</h2>
+      <Button onClick={() => { setVisible(!visible) }}><Icon type={"plus"}/>Add</Button>
+      <AddProducts visible={visible} />
       <ProductList onDelete={ handleDelete } products={ products }/>
     </div>
   );
